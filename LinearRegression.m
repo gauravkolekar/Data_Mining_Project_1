483e3defb2544d320bc81a2833db4cc0a5c3a1ad
Brijesh Dankhara <brijeshdankhara@gmail.com>
2016-02-22 15:48:01 -0600

    Update LinearRegression.m

--- LinearRegression.m
+++ LinearRegression.m
@@ -8,11 +8,7 @@
 A = load('trainDataXY.txt');
 X = load('testDataX.txt');
 Ytrain = load('Ytrain50.txt');
-
-for i = 1:45
-    AA(1:644,i) = A(2:645,i);
-    plot(A(2:645,i),'rx','Markersize',10)
-end
+AA = A(2:645,:);
 
 B = pinv(AA') * Ytrain'; % 644 x 45 | 45 x 5 = 644 x 5
 Ytest1 = B' * X; % 5 x 644 | 644 x 5 = 5 x 5
