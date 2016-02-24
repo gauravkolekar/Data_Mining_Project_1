d32cbdc167404d2aa0d8edc2e8645b8ea2f39ae0
Brijesh Dankhara <brijeshdankhara@gmail.com>
2016-02-24 16:47:39 -0600

    Update LinearRegression.m

--- LinearRegression.m
+++ LinearRegression.m
@@ -12,7 +12,9 @@ AA = A(2:645,:);
 
 B = pinv(AA') * Ytrain'; % 644 x 45 | 45 x 5 = 644 x 5
 Ytest1 = B' * X; % 5 x 644 | 644 x 5 = 5 x 5
-plot(Ytest1,'rx','Markersize',10)
+[Ytest2value  Ytest2]= max(Ytest1,[],1);
+
 % Ytest1 will give out a 5x5 matrix:
 % The columns are the tested values of vector corresponding to the trained class.
 % If 'i'th value in a column 'j' is maximum, this means jth image is closest to ith number of test class.
+% Ytest2 contains the class numbers of each test image.
