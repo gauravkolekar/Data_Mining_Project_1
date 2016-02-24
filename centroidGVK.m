c659b7a1d25d5139f454966c5b4dc5bc0c511e89
Gaurav Kolekar <gaurav.kolekar@mavs.uta.edu>
2016-02-24 15:46:21 -0600

    added Ytrain200.txt and added new function to find euclidean distance between two vectors

--- centroidGVK.m
+++ centroidGVK.m
@@ -5,20 +5,22 @@ Team Members:
 Gaurav Vivek Kolekar
 Brijesh
 %}
-close all
-clear
 trainDataLoad = load('trainDataXY.txt');
 testDataLoad = load('testDataXY.txt');
-YtrainLoad = load('Ytrain50.txt');
+YtrainLoad50 = load('Ytrain50.txt');
+YtrainLoad200 = load('Ytrain200.txt');
 %reading all the values
 trainData = trainDataLoad(2:end,:);
 testData = testDataLoad(2:end,:);
 %cleaning data
-final_vector = (trainData * YtrainLoad') ./ 9; %B = (A * Ytrain') ./ 9;
+final_vector = (trainData * YtrainLoad50') ./ 9; %B = (A * Ytrain') ./ 9;
 final_answer = zeros(5,5);
+
 for i = 1:5
     temp = testData(:,i);
     for j = 1:5
         final_answer(i,j) = sqrt(sum((final_vector(:,j) - temp) .^ 2));
     end
 end
+
+%final_answer = pdist2(final_vector',testData','euclidean');
