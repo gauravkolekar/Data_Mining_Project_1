6b54e72695cb004faa5b0875a378c1351b70c502
Gaurav Kolekar <gaurav.kolekar@mavs.uta.edu>
2016-02-29 14:40:35 -0600

    commiting ppt file for the project

--- knnGVK.m
+++ knnGVK.m
@@ -5,6 +5,7 @@ Team Members:
 Gaurav Vivek Kolekar
 Brijesh Dhankara
 %}
+
 trainDataLoad = load('trainDataXY.txt');
 testDataLoad = load('testDataXY.txt');
 YtrainLoad50 = load('Ytrain50.txt');
@@ -16,14 +17,19 @@ testData = testDataLoad(2:end,:);
 %cleaning data
 
 class_label_matrix = [1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3 3 4 4 4 4 4 4 4 4 4 5 5 5 5 5 5 5 5 5];
-prompt = 'What is the value of K? ';
-K = input(prompt);
+
+K = input('What is the value of K? ');
 distance_vector = zeros(5,45);
 for i = 1:5
     for j = 1:45
         distance_vector(i,j) = norm(testData(:,i) - trainData(:,j));
     end
 end
+[temp,temp_indexes] = sort(distance_vector');
+sorted_matrix = temp';
+sorted_indexes = temp_indexes';
+vote3nn = zeros(5,3);
+vote5nn = zeros(5,5);
 
 switch K
     case 1
@@ -32,8 +38,26 @@ switch K
             indexes(i) = find(distance_vector(i,:)==minimum(i),1);
             final_classes(1,i) = class_label_matrix(1,indexes(i));
         end
+        disp('The 5 queries belong to the following classes using 1-nn:');
+        disp(final_classes);
     case 3
-        disp('3nn')
+        for i = 1:5
+            for j = 1:3
+                vote3nn(i,j) = class_label_matrix(1,sorted_indexes(i,j));
+            end
+            
+        end 
+        final_classes3nn = mode(vote3nn,2);
+        disp('The 5 queries belong to the following classes using 3-nn:');
+        disp(final_classes3nn');
     case 5
-        disp('5nn')
+        for i = 1:5
+            for j = 1:5
+                vote5nn(i,j) = class_label_matrix(1,sorted_indexes(i,j));
+            end
+            
+        end 
+        final_classes5nn = mode(vote5nn,2);
+        disp('The 5 queries belong to the following classes using 5-nn:');
+        disp(final_classes5nn');
 end
